daa59906dfe33eb9490af03e2f4420f3ad3e3e22
Weeks4272 <78891849+Weeks4272@users.noreply.github.com>
2026-03-16 14:58:09 +0000

    Calc setup

---
+++ Personal Projects/CalcuLATER/base.py
@@ -0,0 +1,18 @@
+print(f"Give me your first number of the equation.")
+
+x = input()
+
+print(f"Okay now the second number please.")
+
+y = input()
+
+#test for variable numbers
+#print(f"{x} and {y}")
+
+#Operator selector 
+
+print(f"What symbol are you using?")
+
+op = input()
+
+if op == str("x"):
